98552fd8b5a6b821ecfe18fd9964b7739e76a4d5
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-06-04 17:06:49 +0200

    Scripts:Some logique to display datas

--- assets/scripts/pieces.js
+++ assets/scripts/pieces.js
@@ -10,23 +10,51 @@ getProductsDatas.then(resolve => resolve.json())
 
     for (const data in dataSet) {
 
+        let dataWrapper = document.createElement('article');
+        let dataName = document.createElement('h2');
+        let dataImg = document.createElement('img');
+        let dataPrice = document.createElement('span');
+        let dataDesc = document.createElement('p');
+        let dataDispo = document.createElement('div');
+
 
         //Afficher les infos des objects dans le tableau retour de données
         for (let values in dataSet[data]) {
 
-            let dataWrapper = document.createElement('article');
+            
+            if (dataSet[data].disponibilite) {
+
+                dataSet[data].disponibilite = 'oui';
+                
+            }
+
+            else {
+
+                dataSet[data].disponibilite = 'non';
+            }
+
+        
+            dataWrapper.setAttribute('data-id',`${dataSet[data].id}`);
+           
+            dataName.textContent = `${dataSet[data].nom}`;
+            dataPrice.textContent = `${dataSet[data].prix} euros`;
+            dataImg.setAttribute('src',`${dataSet[data].image}`);
+            dataDesc.textContent = `${dataSet[data].description}`;
+            dataDispo.textContent = `Dispo: ${dataSet[data].disponibilite}`;
+
             dataWrapper.classList.add('datas');
 
-            dataWrapper.textContent = `${values}:${dataSet[data][values]} `;
+            // Push Datas one by One
+            dataWrapper.append(dataName,dataDesc,dataDispo,dataPrice,dataImg);
 
+            //Push Global Datas
             document.querySelector('.filtres').append(dataWrapper);
 
-
-            console.log(`${values} : ${dataSet[data][values]}`);
+            // Print Console Datas
+            console.table(`${values} : ${dataSet[data][values]}`);
 
         }
 
     }
         
 });
-
