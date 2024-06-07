f9c2692cad5d2022b907f5d028fa227d2dbddbfb
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-06-07 11:48:07 +0200

    display categorie data + add CSS class +  UI

--- assets/scripts/pieces.js
+++ assets/scripts/pieces.js
@@ -5,8 +5,8 @@ getProductsDatas.then(resolve => resolve.json())
 .then(
     (dataSet) => {
 
-    //Afficher premiere infos du jeux
-    console.log(dataSet);
+    //Display in Console datas
+    // console.log(dataSet);
     console.log(dataSet[0]);
 
     for (const data in dataSet) {
@@ -17,6 +17,7 @@ getProductsDatas.then(resolve => resolve.json())
         let dataPrice = document.createElement('span');
         let dataDesc = document.createElement('p');
         let dataDispo = document.createElement('div');
+        let dataCat = document.createElement('span');
     
         //Afficher les infos des objects dans le tableau retour de données
         for (let values in dataSet[data]) {
@@ -34,20 +35,32 @@ getProductsDatas.then(resolve => resolve.json())
             }
 
 
-
+            // Set Datas
             dataWrapper.classList.add('product');
             dataWrapper.setAttribute('data-id',`${dataSet[data].id}`);
-           
-            dataName.textContent = `${dataSet[data].nom}`;
-            dataPrice.textContent = `Prix: ${dataSet[data].prix} ${dataSet[data] < 35 ? "$" : "$$$"}`;
-            dataImg.setAttribute('src',`${dataSet[data].image}`);
-            dataDesc.textContent = `${dataSet[data].description}`;
-            dataDispo.textContent = `Disponible: ${dataSet[data].disponibilite}`;
 
+            dataName.classList.add('product_name');
+            dataName.textContent = `${dataSet[data].nom}`;
             
 
-            // Push Datas one by One
-            dataWrapper.append(dataImg,dataName,dataDesc,dataPrice,dataDispo);
+            dataPrice.classList.add('product_price');
+            dataPrice.textContent = `Prix: ${dataSet[data].prix} ${dataSet[data] < 35 ? "$" : "$$$"}`;
+
+            dataImg.classList.add('product_img');
+            dataImg.setAttribute('src',`${dataSet[data].image}`);
+
+            dataDesc.classList.add('product_desc');
+            dataDesc.textContent = `${dataSet[data].description}`;
+
+            dataDispo.classList.add('product_avail');
+            dataDispo.textContent = `Disponible: ${dataSet[data].disponibilite}`;
+
+            dataCat.classList.add('product_cat');
+            dataCat.textContent = `${dataSet[data].categorie}`;
+
+            
+            // Push Datas in DOM
+            dataWrapper.append(dataCat,dataImg,dataName,dataDesc,dataPrice,dataDispo);
 
             //Push Global Datas
             document.querySelector('.fiches').append(dataWrapper);
